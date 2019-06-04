47fa83ed949bddbacf200f48bc1e61b4becb1abf
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-06-04 13:14:47 +0200

    buttons matthias

--- interm 22222/js/script.js
+++ interm 22222/js/script.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
 	var shop = $(".shop_b");
 	var fig =$("figure");
 	var info= $(".info");
+	var increase = $(".btn-inc")
+	var decrease = $(".btn-dec")
+	var remove = $(".delete")
+	var amou = $(".amount")
 
 	console.log(info);
 
@@ -23,12 +27,38 @@ $(document).ready(function() {
 		$(push[i]).on('click', function() {
 
 			$(fig[i]).css("display", "block");
+			$(".button-container").css("display", "block");
    			$(shop[i]).attr("src", items[i].pic);
    			$(info[i]).text(items[i].name+"    € "+items[i].price);
+
 		})
+		var counter = [1,1,1,1,1,1];
+		$(increase[i]).click(function(){
+			$(amou[i]).text(++counter[i])
+		});
+
+		$(decrease[i]).click(function(){
+    		$(amou[i]).text(--counter[i]);
+    		if (counter[i] <= 0) {
+    			$(fig[i]).css("display", "none");
+    		}
+ 		 });
+
+		$(remove[i]).click(function(){
+			$(fig[i]).css("display", "none");
+		});
 	
 	}
 
+
+/*var counter = 0;
+$(".btn-inc").click(function(){
+    $(".qty").attr("placeholder", ++counter);
+      });
+	
+$(".btn-dec").click(function(){
+    $(".qty").attr("placeholder", --counter);
+  });*/
 
 
 
