8d12bdc177544cf368fc488321327adaf638edbd
Matthias Bruckbeck <MBruckbeck@gmx.at>
2019-06-04 09:54:55 +0200

    matthias

---
+++ basic/js/basic.js
@@ -0,0 +1,18 @@
+$("#bell").click(function(){
+  alert("You clicked on the Bell.");
+});
+
+$("#box").click(function(){
+  alert("You clicked on the Box.");
+});
+
+$("#santa").click(function(){
+  alert("You clicked on Santa.");
+});
+
+
+$("#santa").click(function(){
+ $(this).remove();
+});
+
+
